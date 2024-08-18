8c0fb37ef4f163cd53c4d798fd3029bdf3af2a26
DJ Hemath <djhemath@gmail.com>
2024-08-18 17:11:06 +0530

    feat: update node data of FilePicker

--- src/components/custom_nodes/FilePicker/FilePicker.tsx
+++ src/components/custom_nodes/FilePicker/FilePicker.tsx
@@ -1,13 +1,46 @@
-import { Handle, Position } from "@xyflow/react";
+import { Handle, NodeProps, Position, useReactFlow } from "@xyflow/react";
 import { FilePickerLabel } from "./FilePicker.styled";
 import { CustomNodeContainer, CustomNodeInnerContainer, CustomNodeTitle, CustomNodeHelperText } from "../CustomNode.styled";
+import { useState } from "react";
 
-export default function FilterPicker() {
+export default function FilterPicker({ id }: NodeProps) {
+    const [fileName, setFileName] = useState<string | null>(null);
+    const { updateNodeData } = useReactFlow();
+
+    const onFilePicked: React.ChangeEventHandler<HTMLInputElement> = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        if(files) {
+            const reader = new FileReader();
+            reader.onload = (fileReaderEvent) => {
+                const text = fileReaderEvent.target?.result as string;
+
+                if(text) {
+                    try {
+                        updateNodeData(id, {
+                            data: JSON.parse(text),
+                        });
+                        setFileName(files[0].name);
+                    } catch(err) {
+                        // TODO: Handle this
+                        console.log(err);
+                    }
+                }
+            }
+
+            reader.readAsText(files[0]);
+        }
+    }
+
     return (
         <CustomNodeContainer>
             <CustomNodeInnerContainer>
                 <CustomNodeTitle>File picker</CustomNodeTitle>
-                <input type="file" id="file-picker" style={{display: 'none'}} />
+                <input type="file" id="file-picker" style={{display: 'none'}} onChange={onFilePicked}/>
+                {
+                    fileName
+                    ? <small>Picked {fileName}</small>
+                    : null
+                }
                 <FilePickerLabel htmlFor="file-picker">Pick file</FilePickerLabel>
                 <CustomNodeHelperText>Accepts JSON files</CustomNodeHelperText>
             </CustomNodeInnerContainer>
